c723f65089d035b1eafaa0be32526eb8991f7159
Marvellous Ogunleke <marvellousabiola08@gmail.com>
2025-07-20 15:42:36 +0100

    d

--- app/api/inngest/router.js
+++ app/api/inngest/router.js
@@ -2,14 +2,12 @@ import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
 // Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [
+export const { GET, POST, PUT } = serve(inngest,[
     /* your functions will be passed here later! */
     syncUserCreation,
         syncUserUpdation,
         syncUserDeletion
   ],
-});
+);
    
         
